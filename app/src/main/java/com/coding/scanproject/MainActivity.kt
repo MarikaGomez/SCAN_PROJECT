1ed038b50d20c979f992e23ef195815295ba540d
MariKarma <marika.gomez@edu.itescia.fr>
2022-02-02 15:56:45 +0100

    récupération du lien du QR code

--- app/src/main/java/com/coding/scanproject/MainActivity.kt
+++ app/src/main/java/com/coding/scanproject/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.coding.scanproject
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.zxing.integration.android.IntentIntegrator
+import retrofit2.Retrofit
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mQrResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var binding: MainActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Alternative to "onActivityResult", because that is "deprecated"
+        mQrResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK) {
+                val result = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+
+                if(result.contents != null) {
+                    // Do something with the contents (this is usually a URL)
+                    Log.i("MainActivity", result.contents)
+                    //val url = result.contents.toString() + "/"
+                    val retrofit: Retrofit = Retrofit.Builder()
+                        .baseUrl(result.contents)
+                        .build()
+                    Log.i("MainActivity", retrofit.toString())
+                }
+            }
+        }
+
+        // Starts scanner on Create of Overlay (you can also call this function using a button click)
+        startScanner()
+    }
+
+
+    // Start the QR Scanner
+    private fun startScanner() {
+        val scanner = IntentIntegrator(this)
+        // QR Code Format
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        // Set Text Prompt at Bottom of QR code Scanner Activity
+        scanner.setPrompt("QR Code Scanner Prompt Text")
+        // Start Scanner (don't use initiateScan() unless if you want to use OnActivityResult)
+        mQrResultLauncher.launch(scanner.createScanIntent())
     }
 }
