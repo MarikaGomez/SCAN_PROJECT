3beed1a44b04301994b05879abf40d57d0442913
MariKarma <marika.gomez@edu.itescia.fr>
2022-02-04 13:39:42 +0100

    return MealsScanActivity

--- app/src/main/java/com/coding/scanproject/MealsListActivity.kt
+++ app/src/main/java/com/coding/scanproject/MealsListActivity.kt
@@ -9,7 +9,7 @@ import com.coding.scanproject.databinding.ActivityMealsListBinding
 
 class MealsListActivity : AppCompatActivity() {
 
-    private val viewModel: MealsListViewModel by viewModels{MealsListViewModelFactory((application as MealsApplication).repository)}
+    private val viewModel: MealsListViewModel by viewModels { MealsListViewModelFactory((application as MealsApplication).repository) }
     private lateinit var adapter: MealsAdapter
     private lateinit var binding: ActivityMealsListBinding
 
@@ -17,11 +17,12 @@ class MealsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMealsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.deleteMeals()
-//        viewModel.getMeals().observe(this, Observer {  meals ->
-//            adapter = MealsAdapter(meals)
-//            binding.recyclerview.adapter = adapter
-//            binding.recyclerview.layoutManager = LinearLayoutManager(this)
-//        })
+
+        //viewModel.deleteMeals()
+        viewModel.getMeals().observe(this, Observer { meals ->
+            adapter = MealsAdapter(meals)
+            binding.recyclerview.adapter = adapter
+            binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        })
     }
 }
