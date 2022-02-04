473034eba398e93380d8968cbcbd3e273a615a2c
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2022-02-04 10:00:04 +0100

    buttonListMeal

--- app/src/main/java/com/coding/scanproject/MealsAdapter.kt
+++ app/src/main/java/com/coding/scanproject/MealsAdapter.kt
@@ -1,19 +1,31 @@
 package com.coding.scanproject
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.coding.scanproject.entity.MealsData
 import com.coding.scanproject.databinding.ItemMealBinding
+import android.content.Intent
 
 
-class MealsAdapter(private val meals: List<MealsData>) : RecyclerView.Adapter<MealsAdapter.ViewHolder>() {
+
+
+
+class MealsAdapter(private val meals: List<MealsData>) : RecyclerView.Adapter<MealsAdapter.ViewHolder>(){
     class ViewHolder(val binding: ItemMealBinding) :  RecyclerView.ViewHolder(binding.root){
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val meals = meals[position]
-        holder.binding.textView.text = meals.strMeal
+        val intent = Intent(this, DetailsMealActivity::class.java)
+        holder.binding.strMeal.text = meals.strMeal
+        holder.binding.btDetails.setOnClickListener(View.OnClickListener {
+            intent.putExtra("idMeal", holder.binding.idMeal.text);
+            startActivity(intent);
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
